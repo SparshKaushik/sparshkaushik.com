076ccb00e1c8ffbaba11e2fff145a04b3ea276dd
Sparsh Kaushik <contact@Sparshkaushik.com>
2023-12-05 00:02:00 +0530

    Contact form functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  output: "hybrid",
+  integrations: [mdx()],
+  adapter: vercel()
 });
