a72020c5a0d975354889e91c6a572cf6eb97ef54
Sparsh Kaushik <github@sparshkaushik.com>
2025-05-15 13:52:15 +0530

    Refactor Astro configuration and remove unused Icon import in TopBar component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 import mdx from "@astrojs/mdx";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 import icon from "astro-icon";
 
 // https://astro.build/config
